d2e7de9c592002427005d88fcdb63d673cf34081
Nicola R <nicola.rizzi89@hotmail.it>
2021-02-03 12:46:16 +0100

    Milestone 1 +2 + bonus finito.

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+@require __DIR__ . './assets/data-php.php'
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,17 +14,40 @@
 
 <body>
     <div id="app">
+
+        <!-- NAVABR -->
         <div id="navbar">
             <div class="container">
                 <img src="./assets/img/logo.png" alt="">
             </div>
+            <!-- Select Bonus -->
             <select name="generi" id="generi" @change="changeCategory()" v-model="key">
                 <option value="All">All</option>
                 <option v-for="genre in genere" :value="genre">{{genre}}</option>
             </select>
+            <!--xxx Select Bonus -->
         </div>
+        <!--xxx NAVABR -->
+
+        <!-- MAIN SECTION -->
         <div id="main">
+
+            <!-- Php Milestone 1 -->
             <div class="cards container">
+                <?php foreach ($dischi as $disco) { ?>
+                    <div class="cards-item">
+                        <img src="<?php echo $disco['poster'] ?>">
+                        <h3><?php echo $disco['title'] ?></h3>
+                        <p><?php echo $disco['author'] ?></p>
+                        <p><?php echo $disco['genre'] ?></p>
+                        <p><?php echo $disco['year'] ?></p>
+                    </div>
+                <?php  } ?>
+            </div>
+            <!--xxx Php Milestone 1 -->
+
+            <!-- Vuejs Milestone 2-->
+            <!-- <div class="cards container">
                 <div class="cards-item" v-for="item in dischiArray">
                     <img :src="item.poster">
                     <h3> {{item.title}} </h3>
@@ -28,8 +55,10 @@
                     <p> {{item.genre}} </p>
                     <p> {{item.year}} </p>
                 </div>
-            </div>
+            </div> -->
+            <!-- xxx Vuejs Milestone 2-->
         </div>
+        <!--xxx MAIN SECTION -->
     </div>
 
 
